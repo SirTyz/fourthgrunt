c28eac4adc3bb20592dc97c8cae81be46cfd74ce
Tyler Cox <sirtytyz@gmail.com>
2017-02-10 12:00:16 -0600

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fourthgruntApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fourthgruntApp
+ */
+angular.module('fourthgruntApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fourthgruntApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fourthgruntApp
+ */
+angular.module('fourthgruntApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
